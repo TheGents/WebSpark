4e3af35496bce2a00d9e518148dd56c5db2c2a8d
VincentCastig <vincent.castig@gmail.com>
2017-10-14 11:31:49 -0500

    Created index.js and html

---
+++ .env
@@ -0,0 +1,3 @@
+PORT = 3000
+DATABASE_URL = "postgres://gvlgcfngooghpl:369ce9f13f4a09beab1753de2cd52807486883906b501280352d2ee63675b3b5@ec2-107-20-214-99.compute-1.amazonaws.com:5432/d6v9pkah8pvspc?ssl=true"
+secret = "vuvuvuvuvulikeskitkatskitkatskitkats"
---
+++ index.js
@@ -0,0 +1,27 @@
+// import Axios from 'axios';
+
+const dotenv = require('dotenv');
+require('dotenv').config();
+const express = require('express');
+const { json } = require('body-parser');
+const massive = require('massive');
+const cors = require('cors');
+
+
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(express.static('./public'));
+
+
+
+const connectionString = process.env.DATABASE_URL; //Connects to heroku bro
+massive(connectionString).then(db => app.set('db', db));
+
+
+
+app.listen(process.env.PORT, () => { console.log(`Listening on port: ${process.env.PORT}`)});
