5fd73f0952d4f244308c230e49dda08dcbd7405a
VincentCastig <vincent.castig@gmail.com>
2017-10-18 05:31:41 -0500

    Fixed SQL Error in Get_Dudes

--- db/get_dudes.sql
+++ db/get_dudes.sql
@@ -1,6 +1,7 @@
 SELECT user_profile.id, user_profile.first_name, user_profile.school, user_profile.occupation, 
 user_profile.location, user_profile.facebook_pic, user_profile.facebook_auth_id, 
 user_profile.general_bio, user_profile.age, user_profile.photo1, user_profile.photo2, 
-user_profile.photo3, user_profile.photo4, (SELECT AVG(CAST(NULLIF(rating, '0') AS DECIMAL(10,1))) 
+user_profile.photo3, user_profile.photo4, (SELECT ROUND(AVG(CAST(rating AS DECIMAL(10,2) )), 1) 
 from kk_rating WHERE dude_id = user_profile.facebook_auth_id) FROM user_profile WHERE gender = '1';
+
 
