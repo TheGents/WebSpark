425c9b50cf22c0a915849ef993c858d234dff0b0
VincentCastig <vincent.castig@gmail.com>
2017-11-03 15:28:36 -0500

    Debugging for get_dudes.sql

--- db/get_dudes.sql
+++ db/get_dudes.sql
@@ -2,6 +2,6 @@ SELECT user_profile.id, user_profile.first_name, user_profile.school, user_profi
 user_profile.location, user_profile.facebook_pic, user_profile.facebook_auth_id, 
 user_profile.general_bio, user_profile.age, user_profile.photo1, user_profile.photo2, 
 user_profile.photo3, user_profile.photo4, (SELECT ROUND(AVG(CAST(rating AS DECIMAL(10,2) )), 1) 
-from kk_rating WHERE dude_id = user_profile.facebook_auth_id) FROM user_profile WHERE gender = '1';
+from kk_rating WHERE dude_id = user_profile.facebook_auth_id), user_profile.location_score FROM user_profile WHERE gender = '1';
 
 
